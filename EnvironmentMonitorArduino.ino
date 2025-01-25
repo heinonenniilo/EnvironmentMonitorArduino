a4bbcf8b7cd37e23ff08abfc52498e7bea498641
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-01-25 11:34:19 +0200

    Testing

--- EnvironmentMonitorArduino.ino
+++ EnvironmentMonitorArduino.ino
@@ -33,6 +33,21 @@
 #include "TMP36Sensor.h"
 #include "DS18B20Sensor.h"
 
+// Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define USE_DISPLAY 1 // Uncomment in order not to use display
+
+#ifdef USE_DISPLAY
+  Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+#endif
+
+
+
 // Definitions
 // #define CP2102 1
 #define ESPDUINO 1
@@ -697,8 +712,25 @@ void setup()
     } 
   #endif
 
-  Logger.Info("Establish connection");
+  // Display
 
+  #ifdef USE_DISPLAY
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;);
+    }
+    Logger.Info("DISPLAY INITED");
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    // Display static text
+    display.println("App initing");
+    display.display();
+    delay(2000);     
+  #endif
+
+  Logger.Info("Establish connection");
   establishConnection();
 }
 
